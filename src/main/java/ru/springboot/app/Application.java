3ae724db7fe0b9d9787a335ee48b04d1d62799e2
Rinat Gazimov <gazimov19@gmail.com>
2020-10-15 22:20:03 +0300

    fixes

--- src/main/java/ru/springboot/app/Application.java
+++ src/main/java/ru/springboot/app/Application.java
@@ -6,17 +6,24 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.springboot.app.repository.UserRepository;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"ru.springboot.app.model"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
 }
